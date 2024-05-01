7a33242716e080fee5d63bc74b66a08b61b57288
Yassir Oumars <oumyassire555@gmail.com>
2024-05-01 13:46:20 +0100

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,32 @@
+import { Route, Routes } from 'react-router-dom'
+import './App.css'
+import Navbar from './components/Navbar'
+import Home from './pages/Home'
+import Register from './pages/Register'
+import Login from './pages/Login'
+import axios from 'axios'
+import {Toaster} from 'react-hot-toast'
+import { UserContextProvider } from '../context/userContext'
+import Dashboard from './pages/Dashboard'
+
+axios.defaults.baseURL = 'http://localhost:8000'
+axios.defaults.withCredentials = true
+
+
+function App() {
+  return (
+    <UserContextProvider>
+      <Navbar/>
+      <Toaster position='bottom-right' toastOptions={{duration: 2000}}  />
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/register' element={<Register/>} />
+        <Route path='/login' element={<Login/>} />
+        <Route path='/dashboard' element={<Dashboard/>} />
+
+      </Routes>
+    </UserContextProvider>
+  )
+}
+
+export default App
